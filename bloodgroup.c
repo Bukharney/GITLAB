ca6d3e2479ae232b7937f1094dfa8d3e53e7cff7
Jirapat <bukharney@gmail.com>
2023-09-20 15:52:18 +0700

    Change in bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -2,11 +2,11 @@
 #include <string.h>
 void main()
 {
-  char Name[50], G[5], answer[10];
+  char Name[40], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your name? ");
+    printf("What is your Name? ");
     scanf("%s", Name);
     do
     {
